ac6e46c14b2eb72e0a255ff879aae03785b44fa7
Бадамгарав Орги <24LP1595@G401-CC0119.local>
2024-10-31 12:59:26 +0800

    CRUD

--- back-end/index.js
+++ back-end/index.js
@@ -8,15 +8,40 @@
 
 // server.listen(3333);
 
-import express from express;
+import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.json());
 
-const port=1111;
+const port = 1111;
 
+const users = [
+  { name: "Dashka", age: 25 },
+  { name: "Bold", age: 30 },
+];
 
-app.listen(port, ()=>{
+app.get("/users", (request, response) => {
+  response.send(users);
+});
 
-    console.log (`Server Ajillaj bna ${port}`)
+app.post("/users", (request, response) => {
+  const { name, age } = request.body;
 
-})
+  console.log(request.body);
+  response.send("Hello received POST request");
+});
+
+app.put("/users", (request, response) => {
+  console.log(request.body);
+  response.send("Hello received PUT request");
+});
+
+app.delete("/users", (request, response) => {
+  console.log(request.body);
+  response.send("Hello received DELETE request");
+});
+
+app.listen(port, () => {
+  console.log(`Server Ajillaj bna ${port}`);
+});
